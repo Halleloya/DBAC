f4c06975bdc7b93862e53f27ff85d6cce80fe616
DEEPAKRAJ DHARMAPURI SELVAKUMAR <deepak.rajds2014@gmail.com>
2021-10-28 23:47:33 -0400

    Adding deployments

--- deployments/user_init.sh
+++
@@ -1,9 +0,0 @@
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
-  db.createUser({
-    user: "$MONGO_INITDB_ROOT_USERNAME",
-    pwd: "$MONGO_INITDB_ROOT_PASSWORD",
-    roles: [
-      { role: 'readWrite', db: "$MONGO_INITDB_DATABASE" }
-    ]
-  })
-EOF
